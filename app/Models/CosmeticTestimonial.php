4857527e98c774a10b253f9e3f4fa2671a483c27
GeorgeMisael <georgemisaelgantume@gmail.com>
2025-04-25 23:17:11 +0700

    added models and install laravel fillament

--- app/Models/CosmeticTestimonial.php
+++ app/Models/CosmeticTestimonial.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CosmeticTestimonial extends Model
 {
     //
+    protected $fillable = [
+        'name',
+        'message',
+        'photo',
+        'rating',
+        'cosmetic_id',
+    ];
+
+    public function cosmetic(): BelongsTo
+    {
+        return $this->belongsTo(Cosmetic::class, 'cosmetic_id');
+    }
 }
