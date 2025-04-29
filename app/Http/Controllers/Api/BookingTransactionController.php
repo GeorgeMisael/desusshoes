6890d198182136b5ca2ec2c2c611b584b16d108c
GeorgeMisael <georgemisaelgantume@gmail.com>
2025-04-29 19:31:14 +0700

    added API and complete few API

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -2,10 +2,67 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Filament\Resources\BookingTransactionResource;
+use App\Models\Cosmetic;
 use Illuminate\Http\Request;
+use App\Models\BookingTransaction;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingTransactionRequest;
 
 class BookingTransactionController extends Controller
 {
     //
+    public function store(StoreBookingTransactionRequest $request) {
+        try{
+
+            $validatedData = $request->validate();
+
+            if($request->hasFile('proof')){
+                $filePath = $request->file('proof')->store('proofs', 'public');
+                $validatedData['proof'] = $filePath;
+            }
+
+            $products = $request->input('cosmetic_ids');
+            $totalQuantity = 0;
+            $totalPrice = 0;
+            
+            $cosmeticIds = array_column($products, 'id');
+            $cosmetics = Cosmetic::whereIn('id', $cosmeticIds)->get();
+
+            foreach ($products as $product) {
+                $cosmetic = $cosmetics->firstWhere('id', $product['id']);
+                $totalQuantity += $product['quantity'];
+                $totalPrice += $cosmetic->price * $product['quantity'];
+            }
+
+            $tax = 0.11 * $totalPrice;
+            $grandTotal = $totalPrice + $tax;
+
+            // Populate booking transaction data
+            $validatedData['total_amount'] = $grandTotal;
+            $validatedData['total_tax_amount'] = $tax;
+            $validatedData['sub_total_amount'] = $totalPrice;
+            $validatedData['is_paid'] = false;
+            $validatedData['booking_trx_id'] = BookingTransaction::generateUniqueTrxId();
+
+            // Save total quantity in booking transactions
+            $validatedData['quantity'] = $totalQuantity;
+
+            $bookingTransaction = BookingTransaction::create($validatedData);
+
+            foreach($products as $product){
+                $cosmetic = $cosmetics->firstWhere('id', $product['id']);
+                $bookingTransaction->transactionDetails()->create([
+                    'cosmetic_id' => $product['id'],
+                    'quantity' =>$product['quantity'],
+                    'price' => $cosmetic->price,
+                ]);
+            }
+
+            return new BookingTransactionResource($bookingTransaction->load('transactionDetails'));
+
+        }catch(\Exception $e){
+            return response()->json(['message' => 'An error occureed', 'error' => $e->getMessage()], 500);
+        }
+    }
 }
--- app/Http/Controllers/Api/BrandController.php
+++ app/Http/Controllers/Api/BrandController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\BrandApiResource;
 
 class BrandController extends Controller
 {
     //
+    public function index(Request $request){
+
+        $brand = Brand::withCount(['cosmetics']);
+
+        if($request->has('limit')){
+            $brand->limit($request->input('limit'));
+        }
+
+        return BrandApiResource::collection($brand->get());
+
+    }
+
+    public function show(Brand $brand){
+        
+        $brand->load(['cosmetics', 'popularCosmetics']);
+        $brand->loadCount(['cosmetics']);
+
+        return new BrandApiResource($brand);
+
+    }
 }
